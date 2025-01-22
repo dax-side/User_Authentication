a3610603c0badf4dc646a49f4e4c1a91584b978c
dax-side <damolaadegbite0@gmail.com>
2025-01-22 22:45:38 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,27 @@
 const Sequelize = require('sequelize');
-const config = require("./config.json")
+const config = require("./config");
 
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env]; // Get the configuration for the current environment
 
-const sequelize = new Sequelize(config.development);
+// Initialize Sequelize with the configuration
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+  }
+);
 
-module.exports = {sequelize};
+(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  })();
+  
+  module.exports = { sequelize };
