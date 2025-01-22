6840dc1835c593b87dcbe830882a77d3c3eaaf19
dax-side <damolaadegbite0@gmail.com>
2025-01-22 22:44:38 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,48 @@
-{
-  "development": {
-    "username": "root",
-    "password": process.env.DB_Password,
-    "database": "user_authentication",
-    "host": "localhost",
-    "dialect": "mysql"
+// {
+//   "development": {
+//     "username": "root",
+//     "password": "ADEADE2004**ade",
+//     "database": "user_authentication",
+//     "host": "localhost",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+require('dotenv').config(); // Load environment variables from .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_USERNAME,
+    password: null, // Add a test DB password if needed
+    database: 'database_test',
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.DB_USERNAME,
+    password: null, // Add a production DB password if needed
+    database: 'database_production',
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
