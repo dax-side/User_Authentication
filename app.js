671b53b2f6c7b631bf92054725b671bbe0baac3a
dax-side <damolaadegbite0@gmail.com>
2025-01-23 02:04:35 +0100

    Update app.js1.0

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-require('dotenv').config(); // Make sure dotenv is loaded
-
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const { sequelize } = require("./config/db");
-
-// Middleware
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+//
 app.use((req, res, next) => {
   console.log(`Request Method: ${req.method}`);
   console.log(`Request URL: ${req.url}`);
   console.log(`Request body: ${JSON.stringify(req.body)}`);
   next();
 });
-
 // Routes
 app.use("/auth", require("./routes/userRoutes"));
 app.use("/api", require("./routes/orgRoute"));
-
 if (process.env.NODE_ENV !== "test") {
   sequelize
     .sync({ alter: true })
     .then(() => {
-      const port = process.env.PORT || 3000; // Use the environment variable PORT if available, otherwise fallback to 3000
-      console.log(`Using port: ${port}`); // Log to verify the port
-
+      const port = process.env.PORT || 3000;
       app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+        console.log("Server running on port 3000");
       });
     })
-    .catch((err) => console.error("Database connection failed: ", err));
-
+    .catch((err) => console.error("Dabase connection failed: ", err));
   process.on("unhandledRejection", (err) => {
-    console.log(`An error occurred: ${err.message}`);
+    console.log(`An error occured: ${err.message}`);
   });
 }
-
 module.exports = { app };
