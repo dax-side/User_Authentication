c9ea1760b48b6d928879a7cc37d38a7182df6dc1
dax-side <damolaadegbite0@gmail.com>
2025-01-26 00:31:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const cron = require("node-cron");
 const app = express();
 const { sequelize } = require("./config/db");
 const cors = require("cors");
-//Middleware
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-//
+
 app.use((req, res, next) => {
   console.log(`Request Method: ${req.method}`);
   console.log(`Request URL: ${req.url}`);
   console.log(`Request body: ${JSON.stringify(req.body)}`);
   next();
 });
+
 // Routes
 app.use("/auth", require("./routes/userRoutes"));
 app.use("/api", require("./routes/orgRoute"));
 console.log(process.env);
+
 if (process.env.NODE_ENV !== "test") {
   sequelize
     .sync({ alter: true })
@@ -29,19 +32,24 @@ if (process.env.NODE_ENV !== "test") {
         console.log("Server running on port 3000");
       });
     })
-    .catch((err) => console.error("Dabase connection failed: ", err));
+    .catch((err) => console.error("Database connection failed: ", err));
+
   // Cron job to trigger redeployment
   const deployHookUrl =
     "https://api.render.com/deploy/srv-cu8mj2lds78s73bjcar0?key=KWA0m4_WTGk";
+  
   cron.schedule("* * * * *", async () => {
     try {
       const response = await axios.post(deployHookUrl);
       console.log("Deployment triggered", response.data);
     } catch (error) {
-      console.err("Error triggering deployment", error);
+      console.error("Error triggering deployment", error);
     }
+  });
+
   process.on("unhandledRejection", (err) => {
-    console.log(`An error occured: ${err.message}`);
+    console.log(`An error occurred: ${err.message}`);
   });
 }
+
 module.exports = { app };
